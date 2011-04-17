3cc9d2ba193922893efba357fec6c70acc27561c
Maurice Fonk <git@naneau.nl>
2011-04-17 17:01:43 +0200

    adding test case for bursts

---
+++ coffee/test/bursting.coffee
@@ -0,0 +1,66 @@
+# Unit Test for a single promise
+
+# Nodeunit's test case
+testCase = (require 'nodeunit').testCase
+
+# Nyala
+Nyala = require '../lib'
+Promise = Nyala.Promise
+PromiseChain = Nyala.PromiseChain
+
+module.exports = testCase 
+
+    'Promises can be put into a bursts and execute': (test) ->
+        test.expect 4
+        
+        chain = new PromiseChain
+        p1 = new Promise () -> 
+            test.ok true
+            do @keep
+        p2 = new Promise () -> 
+            test.ok true
+            do @keep
+        p3 = new Promise () -> 
+            test.ok true
+            do @keep
+            
+        chain.add p1
+        chain.add p2
+        chain.add p3                
+        
+        chain.broken () -> test.fail 'We should not fail'        
+        chain.kept () ->
+            test.ok true
+            do test.done
+            
+        do chain.execute
+
+    'Promises can be put into a bursts and send results to kept handlers': (test) ->
+        test.expect 7
+
+        chain = new PromiseChain
+        p1 = new Promise () -> 
+            test.ok true
+            @keep 'foo'
+        p1.kept (foo) -> test.equal foo, 'foo'
+        
+        p2 = new Promise () -> 
+            test.ok true
+            @keep 'bar'
+        p2.kept (bar) -> test.equal bar, 'bar'
+        
+        p3 = new Promise () -> 
+            test.ok true
+            @keep 'baz'
+        p3.kept (baz) -> test.equal baz, 'baz'
+        
+        chain.add p1
+        chain.add p2
+        chain.add p3                
+
+        chain.broken () -> test.fail 'We should not fail'        
+        chain.kept () ->
+            test.ok true
+            do test.done
+
+        do chain.execute
---
+++ test/bursting.js
@@ -0,0 +1,74 @@
+(function() {
+  var Nyala, Promise, PromiseChain, testCase;
+  testCase = (require('nodeunit')).testCase;
+  Nyala = require('../lib');
+  Promise = Nyala.Promise;
+  PromiseChain = Nyala.PromiseChain;
+  module.exports = testCase({
+    'Promises can be put into a bursts and execute': function(test) {
+      var chain, p1, p2, p3;
+      test.expect(4);
+      chain = new PromiseChain;
+      p1 = new Promise(function() {
+        test.ok(true);
+        return this.keep();
+      });
+      p2 = new Promise(function() {
+        test.ok(true);
+        return this.keep();
+      });
+      p3 = new Promise(function() {
+        test.ok(true);
+        return this.keep();
+      });
+      chain.add(p1);
+      chain.add(p2);
+      chain.add(p3);
+      chain.broken(function() {
+        return test.fail('We should not fail');
+      });
+      chain.kept(function() {
+        test.ok(true);
+        return test.done();
+      });
+      return chain.execute();
+    },
+    'Promises can be put into a bursts and send results to kept handlers': function(test) {
+      var chain, p1, p2, p3;
+      test.expect(7);
+      chain = new PromiseChain;
+      p1 = new Promise(function() {
+        test.ok(true);
+        return this.keep('foo');
+      });
+      p1.kept(function(foo) {
+        return test.equal(foo, 'foo');
+      });
+      p2 = new Promise(function() {
+        test.ok(true);
+        return this.keep('bar');
+      });
+      p2.kept(function(bar) {
+        return test.equal(bar, 'bar');
+      });
+      p3 = new Promise(function() {
+        test.ok(true);
+        return this.keep('baz');
+      });
+      p3.kept(function(baz) {
+        return test.equal(baz, 'baz');
+      });
+      chain.add(p1);
+      chain.add(p2);
+      chain.add(p3);
+      chain.broken(function() {
+        return test.fail('We should not fail');
+      });
+      chain.kept(function() {
+        test.ok(true);
+        return test.done();
+      });
+      return chain.execute();
+    }
+  });
+}).call(this);
